1a64cb184f90da4b8a833ef7fbbd5412dbe68d29
Balint Alexandru <balintalex737@gmail.com>
2024-01-14 22:40:52 +0200

    feat: add secure route

--- frontend/src/axios/api.js
+++ frontend/src/axios/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.1.2:4000/api',
+  baseURL: process.env.BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
